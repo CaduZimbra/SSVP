6a56b484a4f61debcb2cce620946e5c301ab6a32
João Vieira <pedrinhogv2122@gmail.com>
2026-01-06 16:59:40 -0300

    Initialize balance and add event listeners

--- SSVP/src/scripts/inicio.js
+++ SSVP/src/scripts/inicio.js
@@ -1,1 +1,24 @@
-.
+let valorEmCaixa = 1500.00;
+const elemento = document.getElementById('valorSaldo');
+elemento.innerText = valorEmCaixa;
+
+const botao = document.getElementById('ultimaEscala');
+const abaUltima = document.getElementById('abaUltima');
+
+botao.addEventListener('click', function(){
+    abaUltima.classList.toggle('destaque')
+});
+
+const botaoDois = document.getElementById('proximaEscala');
+const abaProxima = document.getElementById('abaProxima');
+
+botaoDois.addEventListener('click', function(){
+    abaProxima.classList.toggle('destaque')
+});
+
+const botaoTres = document.getElementById('menuLateral');
+const conteudoMenu = document.getElementById('barra');
+
+botaoTres.addEventListener('click',  function(){
+    conteudoMenu.classList.toggle('barraLateral')
+});
