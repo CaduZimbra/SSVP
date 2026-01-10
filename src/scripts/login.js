457cf82a1b09b235131438ea7daef824130c121e
Joao-gv <pedrinhogv2122@gmail.com>
2026-01-10 16:15:16 -0300

    Agora adicionei o javascript de login e registro do Cadu

---
+++ src/scripts/login.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const formLogin = document.querySelector('form');
+
+    formLogin.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        
+        const emailDigitado = document.getElementById('email').value;
+        const senhaDigitada = document.getElementById('password').value;
+
+        
+        const usuarioSalvoTexto = localStorage.getItem('usuarioSSVP');
+
+        
+        if (!usuarioSalvoTexto) {
+            alert('Nenhum usuário cadastrado! Vá em "Registrar-se" primeiro.');
+            return;
+        }
+
+        
+        const usuarioSalvo = JSON.parse(usuarioSalvoTexto);
+
+        
+        if (emailDigitado === usuarioSalvo.emailSalvo && senhaDigitada === usuarioSalvo.senhaSalva) {
+            
+            alert('Bem-vindo de volta, ' + usuarioSalvo.nomeSalvo + '!');
+            window.location.href = '../src/pages/inicio.html'; 
+
+        } else {
+            alert('Email ou senha incorretos!');
+        }
+    });
+});
