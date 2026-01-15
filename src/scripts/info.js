524c6ab4e34eb6cc01148aebe5a2290107b39026
Victor Pedras <victorfrpedras@gmail.com>
2026-01-15 20:20:51 -0300

    Abas funcionando

--- src/scripts/info.js
+++ src/scripts/info.js
@@ -7,3 +7,26 @@ function toggleMenu() {
     menu.style.width = "250px";
   }
 }
+
+const botoes = document.querySelectorAll('.tab-btn');
+const conteudos = document.querySelectorAll('.aba-conteudo');
+
+botoes.forEach(botao => {
+  botao.addEventListener('click', () => {
+
+    // 1. Remove active de todos os botões
+    botoes.forEach(b => b.classList.remove('active'));
+
+    // 2. Fecha todos os conteúdos
+    conteudos.forEach(c => c.classList.remove('aba-aberta'));
+
+    // 3. Ativa o botão clicado
+    botao.classList.add('active');
+
+    // 4. Abre o conteúdo correspondente
+    const idConteudo = botao.getAttribute('data-tab');
+    const conteudo = document.getElementById(idConteudo);
+
+    conteudo.classList.add('aba-aberta');
+  });
+});
